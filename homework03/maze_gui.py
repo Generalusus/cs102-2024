19275e2dbd4b48930cd7939ad9a846ca22fed903
Generalusus <kirill.enderman@gmail.com>
2025-01-27 17:47:35 +0300

    fixed black issues

--- homework03/maze_gui.py
+++ homework03/maze_gui.py
@@ -1,3 +1,5 @@
+"""GUI"""
+
 import tkinter as tk
 from tkinter import messagebox, ttk
 from typing import List
@@ -6,6 +8,7 @@ from maze import add_path_to_grid, bin_tree_maze, solve_maze
 
 
 def draw_cell(x, y, color, size: int = 10):
+    """Draws a cell"""
     x *= size
     y *= size
     x1 = x + size
@@ -14,18 +17,20 @@ def draw_cell(x, y, color, size: int = 10):
 
 
 def draw_maze(grid: List[List[str]], size: int = 10):
+    """Draws the maze"""
     for x, row in enumerate(grid):
         for y, cell in enumerate(row):
             if cell == " ":
-                color = 'White'
+                color = "White"
             elif cell == "■":
-                color = 'black'
+                color = "black"
             elif cell == "X":
                 color = "red"
             draw_cell(y, x, color, size)
 
 
 def show_solution():
+    """checks solutions and draws the shortest one"""
     maze, path = solve_maze(GRID)
     maze = add_path_to_grid(GRID, path)
     if path:
@@ -42,7 +47,7 @@ if __name__ == "__main__":
     GRID = bin_tree_maze(N, M)
 
     window = tk.Tk()
-    window.title('Maze')
+    window.title("Maze")
     window.geometry("%dx%d" % (M * CELL_SIZE + 100, N * CELL_SIZE + 100))
 
     canvas = tk.Canvas(window, width=M * CELL_SIZE, height=N * CELL_SIZE)
@@ -52,4 +57,3 @@ if __name__ == "__main__":
     ttk.Button(window, text="Solve", command=show_solution).pack(pady=20)
 
     window.mainloop()
-
